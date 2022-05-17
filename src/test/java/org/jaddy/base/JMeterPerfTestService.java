b1bc66653836652c0a7a94ed179494764fbbb183
jadams <adamsjt95@gmail.com>
2022-05-17 02:24:37 -0400

    created Random Price method

--- src/test/java/org/jaddy/base/JMeterPerfTestService.java
+++ src/test/java/org/jaddy/base/JMeterPerfTestService.java
@@ -13,20 +13,22 @@ import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
 
 public class JMeterPerfTestService {
 
-
+        private static double generateRandomPrice() {
+            double ranprice = (Math.random() * ((500.00 - 1.00) + 1)) + 1.00;
+            return ranprice;
+        }
 
         @Test
         public void testPerformance() throws IOException {
             TestPlanStats stats = testPlan(
                     threadGroup(2, 10,
                             httpSampler("http://localhost:8080/products")
-                                    .post("{\"name\": \"Amazon Fire\", \"price\": 129.0}", ContentType.APPLICATION_JSON).header("Authorization",
-                                            "Bearer ")
+                                    .post("{\"name\": \"Amazon Fire\", \"price\": " +  generateRandomPrice() + "\t}", ContentType.APPLICATION_JSON).header("Authorization",
+                                            "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxLHRlc3R1czA5QGdtYWlsLmNvbSIsImlzcyI6IkxhdmlzaExhc2hlcyIsImlhdCI6MTY1Mjc2NDYwNCwiZXhwIjoxNjUyODUxMDA0fQ.8OiW3X0Un_j_S_vRHmiTdr95W1BBCTx8xBruq_FXNCQ2ZX-_o1KokroDi8gYuUpwfEqpT2urUJfYwUOzC9CVbw")
                     ),
                     //this is just to log details of each request stats
                     jtlWriter("test" + Instant.now().toString().replace(":", "-") + ".jtl")
             ).run();
             assertThat(stats.overall().sampleTimePercentile99()).isLessThan(Duration.ofSeconds(5));
         }
-
     }
